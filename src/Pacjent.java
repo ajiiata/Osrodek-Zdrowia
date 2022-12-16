eb87a8372f1f0890f8057f225fb5e9610984294a
Agata Jabłońska <ajiatajablonska@gmail.com>
2022-12-16 20:08:56 +0100

    Stworzenie konstruktora przyjmującego wszystkie możliwe pola klasy Pacjent

--- src/Pacjent.java
+++ src/Pacjent.java
@@ -1,9 +1,23 @@
 public class Pacjent {
-    String imiePacjenta;
-    String nazwiskoPacjenta;
-    long nrTelefonu;
-    int idPacjenta;
-    String narodowosc;
-    String miejsceUrodzenia;
+    private int idPacjenta;
+    private String imiePacjenta;
+    private String nazwiskoPacjenta;
+    private long pesel;
+    private long nrTelefonu;
+    private String narodowosc;
+    private String miejsceUrodzenia;
     //adres będzie klasą, bo jest dość złożony, więc lepiej tak
+
+    public Pacjent(int idPacjenta, String imiePacjenta, String nazwiskoPacjenta, long pesel, long nrTelefonu,
+                   String narodowosc, String miejsceUrodzenia) {
+
+        this.idPacjenta = idPacjenta;
+        this.imiePacjenta = imiePacjenta;
+        this.nazwiskoPacjenta = nazwiskoPacjenta;
+        this.pesel = pesel;
+        this.nrTelefonu = nrTelefonu;
+        this.narodowosc = narodowosc;
+        this.miejsceUrodzenia = miejsceUrodzenia;
+
+    }
 }
