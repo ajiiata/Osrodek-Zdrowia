a6ba41836c93b0cb05853634fdec4ab623fdb234
Agata Jabłońska <ajiatajablonska@gmail.com>
2022-12-17 00:16:30 +0100

    Dodanie pól w klasie Adres, stworzenie konstruktora, który przyjmuje wszystkie pola klasy Adres

--- src/Adres.java
+++ src/Adres.java
@@ -1,2 +1,21 @@
+import java.util.Optional;
+
 public class Adres {
+    private String ulica;
+    private int nrDomu;
+    private Optional nrMieszkania = Optional.ofNullable(null);
+    private String miejscowosc;
+    private String kodPocztowy; // kod pocztowy zmodelowac przez wyrazenia regularne
+
+    public Adres(String ulica, int nrDomu, Optional nrMieszkania, String miejscowosc, String kodPocztowy) {
+        this.ulica = ulica;
+        this.nrDomu = nrDomu;
+        this.nrMieszkania = nrMieszkania;
+        this.miejscowosc = miejscowosc;
+        this.kodPocztowy = kodPocztowy;
+    }
+
+
+
+
 }
