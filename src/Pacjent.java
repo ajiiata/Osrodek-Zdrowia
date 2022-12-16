71d05de92a9a9b1fbf53ad1f77485e6c192fbee5
Agata Jabłońska <ajiatajablonska@gmail.com>
2022-12-16 19:59:28 +0100

    Stworzenie pól w klasie Pacjent

---
+++ src/Pacjent.java
@@ -0,0 +1,9 @@
+public class Pacjent {
+    String imiePacjenta;
+    String nazwiskoPacjenta;
+    long nrTelefonu;
+    int idPacjenta;
+    String narodowosc;
+    String miejsceUrodzenia;
+    //adres będzie klasą, bo jest dość złożony, więc lepiej tak
+}
