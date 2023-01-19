ab576cfe59ea5de9c412ccb7bdba6101f98c9773
Agata Jabłońska <ajiatajablonska@gmail.com>
2023-01-19 09:19:16 +0100

    Utworzenie repozytorium pacjentow

---
+++ src/RepozytoriumPacjentow.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class RepozytoriumPacjentow {
+    private HashMap<Integer, Pacjent> pacjenci = new HashMap<Integer, Pacjent>();
+
+    public RepozytoriumPacjentow(String bazaDanych) {
+
+    }
+}
