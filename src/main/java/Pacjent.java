5f08e8ba82f7ed10e4ae32c2b717191393a7c459
WruszkaHanuszka <hanias2002@wp.pl>
2023-01-22 16:27:27 +0100

    Poprawka w klasie Zaswiadczenie_Lekarskie

--- src/main/java/Pacjent.java
+++ src/main/java/Pacjent.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ListIterator;
--- src/main/java/Zaswiadczenie_Lekarskie.java
+++ src/main/java/Zaswiadczenie_Lekarskie.java
@@ -1,10 +1,8 @@
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Zaswiadczenie_Lekarskie {
-    // czy moze jakis typZaswiadczenia - tu wybieramy: skierowanie, zwolnienie?
-    String skierowanie;
-    String zwolnienie;
-    Date dataWystawienia;
+    String typ_zaswiadczenia;
+    LocalDate dataWystawienia;
     private String miejscowosc;
     private String imiePacjenta;
     private String nazwiskoPacjenta;
@@ -12,19 +10,22 @@ public class Zaswiadczenie_Lekarskie {
     private String podmiotWystawiajacy = "Osrodek Zdrowia HAI";
     private String dodatkoweInformacje;
 
-    public Zaswiadczenie_Lekarskie(String imiePacjenta, String nazwiskoPacjenta, long pesel, String miejscowosc, String dodatkoweInformacje) {
+    public Zaswiadczenie_Lekarskie(String typ_zaswiadczenia, String imiePacjenta, String nazwiskoPacjenta, long pesel, String miejscowosc, String dodatkoweInformacje) {
+        this.typ_zaswiadczenia = typ_zaswiadczenia;
         this.imiePacjenta = imiePacjenta;
         this.nazwiskoPacjenta = nazwiskoPacjenta;
         this.pesel = pesel;
         this.miejscowosc = miejscowosc;
         this.dodatkoweInformacje = dodatkoweInformacje;
         this.podmiotWystawiajacy = "Osrodek Zdrowia HAI";
+        this.dataWystawienia = LocalDate.now();
+    }
 
-        // no chyba ze wpisujemy tylko idPacjenta i to uzupelnia i oddaje automatycznie jego dane...
-
-        // data do rozkminienia
-
-        // tu tez sie przyda jaki lekarz wystawil zaswiadczenie
+    void wyswietlZaswiadczenie() {
+        System.out.println("Utworzono zaswiadczenie lekarskie: \n" + typ_zaswiadczenia +
+                "\n Podmiot wystawiający: " + podmiotWystawiajacy + "\nData: " + dataWystawienia + " Miejscowość: " + miejscowosc +
+                "\n Imię i Nazwisko: " + imiePacjenta + " " + nazwiskoPacjenta + "\nPesel: " + pesel +
+                "Informacje: " + dodatkoweInformacje);
     }
 
 
